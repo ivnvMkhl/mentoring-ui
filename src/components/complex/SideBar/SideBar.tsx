0264c5fdbc0b354178caa26ba089f55c7f0b7068
Юра Маринин <uramarinin@MacBook-Air-Ura.local>
2023-11-02 20:26:10 +0400

    add active state for MenuItem

--- src/components/complex/SideBar/SideBar.tsx
+++ src/components/complex/SideBar/SideBar.tsx
@@ -5,11 +5,27 @@ import { MenuItem } from '../MenuItem/MenuItem';
 import { Icon } from '../../primitives/Icon/Icon';
 
 const SideBar: FC = () => {
+  const curentPath = window.location.pathname;
   return (
     <div className={styles.sideBar}>
-      <MenuItem label="Ученики" link="/menti-list" icon={<Icon kind="UsergroupAdd" size="l" />} />
-      <MenuItem label="Занятия" link="/lesson-list" icon={<Icon kind="Contacts" size="l" />} />
-      <MenuItem label="Аналитика" link="/analytics" icon={<Icon kind="BarChart" size="l" />} />
+      <MenuItem
+        label="Ученики"
+        link="/menti-list"
+        icon={<Icon kind="UsergroupAdd" size="l" />}
+        active={curentPath === '/menti-list'}
+      />
+      <MenuItem
+        label="Занятия"
+        link="/lesson-list"
+        icon={<Icon kind="Contacts" size="l" />}
+        active={curentPath === '/lesson-list'}
+      />
+      <MenuItem
+        label="Аналитика"
+        link="/analytics"
+        icon={<Icon kind="BarChart" size="l" />}
+        active={curentPath === '/analytics'}
+      />
     </div>
   );
 };
