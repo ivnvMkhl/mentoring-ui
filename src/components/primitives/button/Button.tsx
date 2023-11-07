0e618701a3f0b9fd990de9a50047fe00abb4d604
Юра Маринин <uramarinin@MacBook-Air-Ura.local>
2023-11-07 21:33:45 +0300

    some fixed last commit, fixed path and dependencies

--- src/components/primitives/button/Button.tsx
+++ src/components/primitives/button/Button.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button as AntdButton } from "antd";
-import type {ButtonProps} from "antd"
+import React from 'react';
+import { Button as AntdButton } from 'antd';
+import type { ButtonProps } from 'antd';
 const Button: React.FC<ButtonProps> = (props) => <AntdButton {...props}></AntdButton>;
 
-export {Button};
+export { Button };
