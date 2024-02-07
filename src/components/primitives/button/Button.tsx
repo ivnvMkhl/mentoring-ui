01ac7bfde5f20ffdc1e4e23ce32af5d851d9603c
Юра Маринин <uramarinin@MacBook-Air-Ura.local>
2024-02-07 19:40:41 +0400

    some changed

--- src/components/primitives/button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Button as AntdButton } from "antd";
-import type {ButtonProps} from "antd"
-const Button: React.FC<ButtonProps> = (props) => <AntdButton {...props}></AntdButton>;
-
-export {Button};
--- src/components/primitives/input/Input.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Input as AntdInput} from "antd";
-import type {InputProps} from "antd";
-const Input: React.FC<InputProps> = (props: InputProps) => <AntdInput {...props}></AntdInput>;
-export {Input};
